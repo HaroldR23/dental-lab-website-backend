7111b0d984fe3492e696edd2d2eb13b26f28dc65
HaroldR23 <harold.rodriguez@ioet.com>
2024-06-27 01:24:02 -0300

    feat: implement get_by_name adapter method

--- adapters/src/repositories/sql/sql_product_adapter.py
+++ adapters/src/repositories/sql/sql_product_adapter.py
@@ -11,28 +11,40 @@ class SQLProductRepository(ProductRepository):
 
     def create(self, product: Product):
         try:
+            print(product, "product in adapter layer--<<<")
             product_to_create = ProductRecord(
                 name=product.name,
                 price=product.price,
                 img_url=product.img_url
             )
-            with self.session as session:
-                self.session.add(product_to_create)
-                id = product_to_create.id
-                self.session.commit()
-                return Product(
-                    **{
-                        "id": id,
-                        **product._asdict()
-                    }
-                )
-        except Exception as e:
+            self.session.add(product_to_create)
+            self.session.flush()
+            id = product_to_create.id
+            self.session.commit()
+            return Product(
+                    id=id,
+                    name=product.name,
+                    price=product.price,
+                    img_url=product.img_url
+            )
+        except Exception:
             self.session.rollback()
             raise ProductRepositoryException(method="create")
 
     def get_by_name(self, product_name: str):
-        raise NotImplementedError
-    
+        try:
+            product = self.session.query(ProductRecord).filter(ProductRecord.name == product_name).first()
+            if product:
+                return Product(
+                    id=product.id,
+                    name=product.name,
+                    price=product.price,
+                    img_url=product.img_url
+                )
+            return None
+        except Exception:
+            raise ProductRepositoryException(method="get_by_name")
+
     def list(self):
         raise NotImplementedError
     
