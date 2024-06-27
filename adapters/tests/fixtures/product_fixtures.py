946b34b16c51c18aa5f725146f905677c41f975e
HaroldR23 <harold.rodriguez@ioet.com>
2024-06-27 01:22:42 -0300

    test: implement tests for get_by_name adapter method

--- adapters/tests/fixtures/product_fixtures.py
+++ adapters/tests/fixtures/product_fixtures.py
@@ -4,16 +4,6 @@ from adapters.src.repositories.sql.sql_product_adapter import SQLProductReposito
 from core.src.models.product import Product
 
 @pytest.fixture
-def sql_product_repository(mock_session) -> SQLProductRepository:
-    mock_sql_product_repository: SQLProductRepository = SQLProductRepository(mock_session)
-    return mock_sql_product_repository
-
-@pytest.fixture
-def sql_product_repository_bad_session(mock_bad_session) -> SQLProductRepository:
-    mock_sql_product_repository: SQLProductRepository = SQLProductRepository(mock_bad_session)
-    return mock_sql_product_repository
-
-@pytest.fixture
 def mock_product_model():
     product = Product(
         name="Test Product",
--- adapters/tests/repositories/sql/product/test_create_product_sql_adapter.py
+++ adapters/tests/repositories/sql/product/test_create_product_sql_adapter.py
@@ -3,17 +3,26 @@ import pytest
 from adapters.src.exceptions.repository.product import ProductRepositoryException
 from adapters.src.repositories.sql.sql_product_adapter import SQLProductRepository
 from core.src.models.product import Product
+from adapters.src.repositories.sql.config_db.session_manager import Session
 
 def test_sql_create_product_adapter_should_return_product_model(
-        sql_product_repository: SQLProductRepository, 
-        mock_product_model: Product
+        mock_product_model: Product,
+        mock_session: Session
     ):
+    # Arrange
+    sql_product_repository = SQLProductRepository(session=mock_session)
+    # Act
     created_product = sql_product_repository.create(product=mock_product_model)
+    # Assert
     assert isinstance(created_product, Product)
 
 def test_sql_create_product_adapter_should_raise_an_exception_when_something_went_wrong(
-        sql_product_repository_bad_session: SQLProductRepository, 
-        mock_product_model: Product
+        mock_product_model: Product,
+        mock_bad_commit_session: Session
     ):
-    with pytest.raises(ProductRepositoryException):
-        sql_product_repository_bad_session.create(product=mock_product_model)
+    # Arrange
+    sql_product_repository = SQLProductRepository(session=mock_bad_commit_session)
+    error_message="Something went wrong trying to create the Product"
+    # Act and Assert
+    with pytest.raises(ProductRepositoryException, match=error_message):
+        sql_product_repository.create(product=mock_product_model)
