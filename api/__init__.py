8658f6dae17d757395cf98a4c6999cbb5b9bb215
HaroldR23 <harold.rodriguez@ioet.com>
2024-07-08 00:04:36 -0300

    add CORS middleware

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,14 @@
+import os
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from adapters.src.repositories.sql import SessionManager, SQLConnection
 from api.src.routers import appointment_router, index_router, product_router
+
+origins = os.environ.get("CORS_ORIGINS", "*").split(",")
 
 
 @asynccontextmanager
@@ -17,5 +21,12 @@ async def lifespan(app: FastAPI) -> AsyncGenerator:
 
 def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=False,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(index_router)
     return app
