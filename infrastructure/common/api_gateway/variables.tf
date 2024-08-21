809c0e7d1d03a1560f0ece6d5e5d2d9a894f161b
HaroldR23 <harold.rodriguez@ioet.com>
2024-08-21 12:10:18 -0300

    feat: create api gateway resources (IaC)

---
+++ infrastructure/common/api_gateway/variables.tf
@@ -0,0 +1,14 @@
+variable "lambda_uri" {
+    description = "URI of the Lambda function to integrate with the API Gateway"
+    type = string
+}
+
+variable "stage_name" {
+    description = "Name of the stage to deploy the API Gateway to"
+    type = string
+}
+
+variable "lambda_name" {
+    description = "Name of the Lambda function to integrate with the API Gateway"
+    type = string
+}
